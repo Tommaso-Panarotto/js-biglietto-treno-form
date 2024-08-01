b12d61541264c4b50699882a201c938ed6acdd27
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-01 16:28:48 +0200

    added step

---
+++ js/form.js
@@ -0,0 +1,8 @@
+/*
+0. Recupero gli elementi dal DOM
+1. Chiedo all'utente tutti i dati
+3. verifico i dati
+4. Calcolo il costo del biglietto
+5. Applico al costo del biglietto eventuali sconti
+6. Inserisco gli elementi nel DOM
+ */
