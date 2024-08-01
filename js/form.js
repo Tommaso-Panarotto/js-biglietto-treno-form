e66fae2d37c82c1737fbb908d8b5a39ec779a654
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-01 16:31:04 +0200

    recall DOM elements

--- js/form.js
+++ js/form.js
@@ -6,3 +6,15 @@
 5. Applico al costo del biglietto eventuali sconti
 6. Inserisco gli elementi nel DOM
  */
+
+//Fase preparatoria
+const resultkilometres = document.getElementById('kilometres');
+const resultName = document.getElementById('full-name')
+const resultAge = document.getElementById('age');
+const resultPrice = document.getElementById('total-price');
+const resultSale = document.getElementById('sale');
+const userName = document.getElementById('NAME');
+const userKilometres = document.getElementById('KMS');
+const userAge = document.querySelector('select');
+const userGenerate = document.getElementById('generate-btn');
+const userReset = document.getElementById('reset-btn')
